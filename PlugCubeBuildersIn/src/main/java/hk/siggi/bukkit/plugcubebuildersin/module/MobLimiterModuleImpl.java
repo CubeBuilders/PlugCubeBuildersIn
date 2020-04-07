e1a097300eb26027d278a9e9100df42620bf4f6e
Siggi88 <7286178+Siggi88@users.noreply.github.com>
2020-04-07 21:28:23 +0800

    Change up the MobLimiter a bit

--- PlugCubeBuildersIn/src/main/java/hk/siggi/bukkit/plugcubebuildersin/module/MobLimiterModuleImpl.java
+++ PlugCubeBuildersIn/src/main/java/hk/siggi/bukkit/plugcubebuildersin/module/MobLimiterModuleImpl.java
@@ -1,6 +1,8 @@
 package hk.siggi.bukkit.plugcubebuildersin.module;
 
+import hk.siggi.bukkit.plugcubebuildersin.MobUtility;
 import hk.siggi.bukkit.plugcubebuildersin.PlugCubeBuildersIn;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -13,8 +15,9 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Dispenser;
-import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -28,50 +31,62 @@ import org.bukkit.inventory.ItemStack;
 public class MobLimiterModuleImpl implements MobLimiterModule, Listener {
 
 	private PlugCubeBuildersIn plugin;
-	private final Set<LivingEntity> trackedEntities = new HashSet<>();
-	private final Map<LivingEntity, String> entityToSpawnedChunk = new HashMap<>();
-	private final Map<String, Set<LivingEntity>> entitiesSpawnedInChunks = new HashMap<>();
+	private final Map<MobRegion, Set<LivingEntity>> entitiesSpawnedInChunks = new HashMap<>();
+	private final Set<LivingEntity> empty = Collections.unmodifiableSet(new HashSet<>());
+	private static final int REGION_SIZE = 3;
+	private static final int REGION_SIZE_Y = 3;
+	private static final int MAX_REGION_Y = 256 / (1 << REGION_SIZE_Y);
 
 	private void clean() {
-		for (Iterator<LivingEntity> it = trackedEntities.iterator(); it.hasNext();) {
-			LivingEntity entity = it.next();
-			if (!entity.isValid()) {
-				String chunkString = entityToSpawnedChunk.get(entity);
-				entityToSpawnedChunk.remove(entity);
-				it.remove();
-				if (chunkString != null) {
-					Set<LivingEntity> entityList = entitiesSpawnedInChunks.get(chunkString);
-					if (entityList != null) {
-						entityList.remove(entity);
-						if (entityList.isEmpty()) {
-							entitiesSpawnedInChunks.remove(chunkString);
-						}
-					}
+		for (Iterator<Map.Entry<MobRegion, Set<LivingEntity>>> regionIt = entitiesSpawnedInChunks.entrySet().iterator(); regionIt.hasNext();) {
+			Map.Entry<MobRegion, Set<LivingEntity>> mapEntry = regionIt.next();
+			Set<LivingEntity> entitySet = mapEntry.getValue();
+			for (Iterator<LivingEntity> entityIt = entitySet.iterator(); entityIt.hasNext();) {
+				LivingEntity entity = entityIt.next();
+				if (!entity.isValid()) {
+					entityIt.remove();
 				}
+			}
+			if (entitySet.isEmpty()) {
+				regionIt.remove();
 			}
 		}
 	}
 
-	private void addEntity(LivingEntity entity, Chunk chunk) {
-		if (trackedEntities.contains(entity)) {
-			return;
+	private void trackEntity(LivingEntity entity, MobRegion chunk) {
+		Set<LivingEntity> entitySet = entitiesSpawnedInChunks.get(chunk);
+		if (entitySet == null) {
+			entitiesSpawnedInChunks.put(chunk, entitySet = new HashSet<>());
 		}
-		trackedEntities.add(entity);
-		String chunkString = getString(chunk);
-		entityToSpawnedChunk.put(entity, chunkString);
-		Set<LivingEntity> entityList = entitiesSpawnedInChunks.get(chunkString);
-		if (entityList == null) {
-			entitiesSpawnedInChunks.put(chunkString, entityList = new HashSet<>());
-		}
-		entityList.add(entity);
+		entitySet.add(entity);
 	}
 
-	private Set<LivingEntity> getEntitiesSpawnedInChunk(String chunkString) {
-		Set<LivingEntity> entityList = entitiesSpawnedInChunks.get(chunkString);
-		if (entityList == null) {
-			return new HashSet<>();
+	private Set<LivingEntity> getEntitiesSpawnedHere(MobRegion region, int regionDistance, int regionDistanceY) {
+		if (regionDistance == 0 && regionDistanceY == 0) {
+			Set<LivingEntity> set = entitiesSpawnedInChunks.get(region);
+			if (set == null) {
+				return empty;
+			}
+			return set;
 		}
-		return entityList;
+		HashSet<LivingEntity> result = new HashSet<>();
+		int minX = region.x - regionDistance;
+		int maxX = region.x + regionDistance;
+		int minY = Math.max(0, region.y - regionDistanceY);
+		int maxY = Math.min(MAX_REGION_Y, region.y + regionDistanceY);
+		int minZ = region.z - regionDistance;
+		int maxZ = region.z + regionDistance;
+		for (int z = minZ; z <= maxZ; z++) {
+			for (int y = minY; y <= maxY; y++) {
+				for (int x = minX; x <= maxX; x++) {
+					Set<LivingEntity> set = entitiesSpawnedInChunks.get(new MobRegion(x, y, z));
+					if (set != null) {
+						result.addAll(set);
+					}
+				}
+			}
+		}
+		return result;
 	}
 
 	private String getString(Chunk chunk) {
@@ -82,15 +97,13 @@ public class MobLimiterModuleImpl implements MobLimiterModule, Listener {
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
 		SpawnReason spawnReason = event.getSpawnReason();
 		boolean skipCheck = false;
-		if (spawnReason == SpawnReason.NATURAL) {
-			StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-			for (StackTraceElement ele : stackTrace) {
-				if (ele.getClassName().endsWith(".CommandSummon")) {
-					skipCheck = true;
-					// allow the /minecraft:summon command to bypass the restrictions
-					// when spawning using that command, it uses SpawnReason.NATURAL for some reason
-					break;
-				}
+		StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+		for (StackTraceElement ele : stackTrace) {
+			if (ele.getClassName().endsWith(".CommandSummon")) {
+				skipCheck = true;
+				// allow the /minecraft:summon command to bypass the restrictions
+				// when spawning using that command, it uses SpawnReason.NATURAL for some reason
+				break;
 			}
 		}
 		if (spawnReason == SpawnReason.DISPENSE_EGG || spawnReason == SpawnReason.SPAWNER_EGG) {
@@ -99,12 +112,13 @@ public class MobLimiterModuleImpl implements MobLimiterModule, Listener {
 			skipCheck = true;
 		}
 		if (!skipCheck) {
-			if (!allowSpawn(event.getLocation(), spawnReason)) {
+			EntityType type = event.getEntity().getType();
+			if (!allowSpawn(type, event.getLocation(), spawnReason)) {
 				event.setCancelled(true);
 				return;
 			}
 		}
-		addEntity(event.getEntity(), event.getLocation().getChunk());
+		trackEntity(event.getEntity(), new MobRegion(event.getLocation()));
 	}
 
 	@EventHandler
@@ -114,14 +128,16 @@ public class MobLimiterModuleImpl implements MobLimiterModule, Listener {
 		if (!(data instanceof Dispenser)) {
 			return;
 		}
-		if (!isSpawnEgg(event.getItem())) {
+		ItemStack item = event.getItem();
+		if (!MobUtility.isEgg(item.getType())) {
 			return;
 		}
 		Dispenser dispenser = (Dispenser) data;
 		BlockFace facing = dispenser.getFacing();
 		Block spawnPoint = block.getRelative(facing);
 		Location pos = spawnPoint.getLocation().add(0.5, 0.0, 0.5);
-		if (!allowSpawn(pos, SpawnReason.DISPENSE_EGG)) {
+		EntityType type = MobUtility.getMob(item.getType());
+		if (!allowSpawn(type, pos, SpawnReason.DISPENSE_EGG)) {
 			event.setCancelled(true);
 		}
 	}
@@ -135,41 +151,55 @@ public class MobLimiterModuleImpl implements MobLimiterModule, Listener {
 		if (item == null) {
 			return;
 		}
-		if (!isSpawnEgg(item)) {
+		if (!MobUtility.isEgg(item.getType())) {
 			return;
 		}
 		Block block = event.getClickedBlock();
 		BlockFace facing = event.getBlockFace();
 		Block spawnPoint = block.getRelative(facing);
 		Location pos = spawnPoint.getLocation().add(0.5, 0.0, 0.5);
-		if (!allowSpawn(pos, SpawnReason.SPAWNER_EGG)) {
+		EntityType type = MobUtility.getMob(item.getType());
+		if (!allowSpawn(type, pos, SpawnReason.SPAWNER_EGG)) {
 			event.setCancelled(true);
 			Player p = event.getPlayer();
 			p.sendMessage(ChatColor.RED + "There are too many mobs here!");
 		}
 	}
 
-	private boolean isSpawnEgg(ItemStack item) {
-		String n = item.getType().name();
-		if (n.endsWith("_SPAWN_EGG") /*1.13 & later*/ || n.equals("MONSTER_EGG") /*1.12 & earlier*/) {
-			return true;
+	private void filter(Set<LivingEntity> entities, boolean monster) {
+		if (monster) {
+			entities.removeIf((entity) -> !(entity instanceof Monster));
+		} else {
+			entities.removeIf((entity) -> (entity instanceof Monster));
 		}
-		return false;
 	}
 
-	private boolean allowSpawn(Location loc, SpawnReason spawnReason) {
-		int limit = 8;
-		int limitNearby = 50;
+	private boolean allowSpawn(EntityType type, Location loc, SpawnReason spawnReason) {
+		int limit = 6;
+		int limitNearby = 30;
+		int nearDistance = 2;
+		int nearDistanceY = 1;
+		boolean isMonster = Monster.class.isAssignableFrom(type.getEntityClass());
+		if (isMonster) {
+			nearDistance = 3;
+			nearDistanceY = 4;
+		}
 		switch (spawnReason) {
 			case BREEDING:
+				limit = 10;
+				limitNearby = 25;
+				nearDistance = 1;
+				nearDistanceY = 1;
+				break;
 			case SPAWNER:
-				limit = 15;
+				limit = 10;
 				limitNearby = -1;
 				break;
 			case BUILD_IRONGOLEM:
 			case BUILD_SNOWMAN:
 			case BUILD_WITHER:
-				limit = 15;
+				limit = 10;
+				limitNearby = -1;
 				break;
 			case CURED:
 			case CUSTOM: // plugins
@@ -195,41 +225,17 @@ public class MobLimiterModuleImpl implements MobLimiterModule, Listener {
 		if (limit == -1) {
 			return true;
 		}
-		Chunk chunk = loc.getChunk();
-		String chunkString = getString(chunk);
-		Set<LivingEntity> entitiesToCount = new HashSet<>();
-		Entity[] entities = chunk.getEntities();
-		for (Entity entity : entities) {
-			if (entity instanceof LivingEntity) {
-				entitiesToCount.add((LivingEntity) entity);
-			}
-		}
-		Set<LivingEntity> spawnedLivingEntities = getEntitiesSpawnedInChunk(chunkString);
-		entitiesToCount.addAll(spawnedLivingEntities);
-		if (entitiesToCount.size() >= limit) {
+		MobRegion region = new MobRegion(loc);
+		Set<LivingEntity> spawnedHere = getEntitiesSpawnedHere(region, 0, 0);
+		filter(spawnedHere, isMonster);
+		if (spawnedHere.size() > limit) {
 			return false;
 		}
-		if (limitNearby == -1) {return true;}
-		int cX = chunk.getX();
-		int cZ = chunk.getZ();
-		for (int chunkZ = cZ - 1; chunkZ <= cZ + 1; chunkZ++) {
-			for (int chunkX = cX - 1; chunkX <= cX + 1; chunkX++) {
-				Chunk chunk2 = chunk.getWorld().getChunkAt(chunkX, chunkZ);
-				if (chunk2.equals(chunk)) {
-					continue;
-				}
-				String chunk2String = getString(chunk);
-				entities = chunk2.getEntities();
-				for (Entity entity : entities) {
-					if (entity instanceof LivingEntity) {
-						entitiesToCount.add((LivingEntity) entity);
-					}
-				}
-				spawnedLivingEntities = getEntitiesSpawnedInChunk(chunk2String);
-				entitiesToCount.addAll(spawnedLivingEntities);
-			}
+		if (limitNearby == -1) {
+			return true;
 		}
-		if (entitiesToCount.size() >= limitNearby) {
+		Set<LivingEntity> spawnedNear = getEntitiesSpawnedHere(region, nearDistance, nearDistanceY);
+		if (spawnedNear.size() >= limitNearby) {
 			return false;
 		}
 		return true;
@@ -251,5 +257,54 @@ public class MobLimiterModuleImpl implements MobLimiterModule, Listener {
 
 	@Override
 	public void tick() {
+	}
+
+	private class MobRegion {
+
+		private final int x;
+		private final int y;
+		private final int z;
+
+		public MobRegion(Location location) {
+			this(location.getBlockX() >> REGION_SIZE, location.getBlockY() >> REGION_SIZE_Y, location.getBlockZ() >> REGION_SIZE);
+		}
+
+		public MobRegion(Block block) {
+			this(block.getX() >> REGION_SIZE, block.getY() >> REGION_SIZE_Y, block.getZ() >> REGION_SIZE);
+		}
+
+		public MobRegion(int x, int y, int z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		@Override
+		public int hashCode() {
+			int hash = 5;
+			hash = 29 * hash + this.x;
+			hash = 29 * hash + this.y;
+			hash = 29 * hash + this.z;
+			return hash;
+		}
+
+		@Override
+		public boolean equals(Object other) {
+			if (!(other instanceof MobRegion)) {
+				return false;
+			}
+			MobRegion o = (MobRegion) other;
+			return x == o.x && y == o.y && z == o.z;
+		}
+
+		public MobRegion getRelative(int x, int y, int z) {
+			int newX = this.x + x;
+			int newY = this.y + y;
+			int newZ = this.z + z;
+			if (newY < 0 || newY >= MAX_REGION_Y) {
+				return null;
+			}
+			return new MobRegion(newX, newY, newZ);
+		}
 	}
 }
